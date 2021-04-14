d323b2be8dcddf7f2ef8400944b5697ee4c5853a
Julia Evans <julia@jvns.ca>
2021-04-14 12:49:47 -0400

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
-wget https://github.com/tmedwards/tweego/releases/download/v2.1.1/tweego-2.1.1-linux-x64.zip
-unzip tweego-2.1.1-linux-x64.zip
-mv tweego /usr/local/bin
-chmod a+x /usr/local/bin/tweego
-mv storyformats /usr/local/bin
-tweego slow-website.twee -o index.html
+./bin/tweego slow-website.twee -o index.html

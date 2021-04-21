f7a61d90dfadffcb7c07c09103c704aa2fd4a4d9
Julia Evans <julia@jvns.ca>
2021-04-21 15:03:17 -0400

    Add case of the problem DNS update

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 ./bin/tweego slow-website.twee common.twee -o slow-website.html
 ./bin/tweego connection-timeout.twee common.twee -o connection-timeout.html
+./bin/tweego problem-dns-update.twee common.twee -o problem-dns-update.html

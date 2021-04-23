aaeb42fb3bdecaf74b5e20251e7a5e1868481146
Melody ✨ <git@melody.dev>
2021-04-23 13:09:43 -0400

    add mac tweego and build script

---
+++ build-mac.sh
@@ -0,0 +1,3 @@
+./bin/tweego-mac slow-website.twee common.twee -o slow-website.html
+./bin/tweego-mac connection-timeout.twee common.twee -o connection-timeout.html
+./bin/tweego-mac problem-dns-update.twee common.twee -o problem-dns-update.html

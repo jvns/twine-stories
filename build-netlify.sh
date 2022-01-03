7c767d4ea3ebc666f7de907e0a8c7beddc65161d
Julia Evans <julia@jvns.ca>
2022-01-03 08:10:47 -0500

    fix

--- build-netlify.sh
+++ build-netlify.sh
@@ -1,7 +1,7 @@
 echo 'building...'
 mkdir -p debug
 
-for i in slow-website problem-dns-update 50ms-request docker-connection
+for i in slow-website problem-dns-update 50ms-request docker-connection connection-timeout
 do
     ./bin/tweego common.twee $i.twee -o $i.html
 done
